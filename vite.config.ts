6c46c95748114176a692d2323e18f21fc9c00d8c
micey <pawel.szumiec98@gmail.com>
2023-04-27 00:46:05 +0200

    import paths aliases update

--- vite.config.ts
+++ vite.config.ts
@@ -3,23 +3,25 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    https: false,
-    hmr: {
-      protocol: "ws",
-      host: "localhost",
-      port: 3000,
+    plugins: [react()],
+    server: {
+        port: 3000,
+        https: false,
+        hmr: {
+            protocol: "ws",
+            host: "localhost",
+            port: 3000,
+        },
     },
-  },
-  resolve: {
-    alias: {
-      "@typings": "/src/types",
-      "@theme": "/src/theme",
-      "@components": "/src/components",
-      "@pages": "/src/pages",
-      "@utils": "/src/utils",
+    resolve: {
+        alias: {
+            "@typings": "/src/types",
+            "@theme": "/src/theme",
+            "@components": "/src/components",
+            "@containers": "/src/containers",
+            "@pages": "/src/pages",
+            "@utils": "/src/utils",
+            src: "/src",
+        },
     },
-  },
 });

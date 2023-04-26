55e76b1887c4a0b900cb505f651f9fe9f31de290
micey <pawel.szumiec98@gmail.com>
2023-04-27 00:45:52 +0200

    added a homepage component

---
+++ src/pages/Homepage/index.tsx
@@ -0,0 +1,13 @@
+import { Box, Heading } from "@chakra-ui/react";
+
+function Homepage() {
+    return (
+        <Box>
+            <Box as="main">
+                <Heading>Homepage</Heading>
+            </Box>
+        </Box>
+    );
+}
+
+export default Homepage;
